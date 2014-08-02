316066de18d50ff126c9ac71163c37fba225f6ee
karthikeyan98 <karthikeyan98@users.noreply.github.com>
2014-08-02 16:07:29 +0530

    Made UI changes and backend code cleanup

--- server.js
+++ server.js
@@ -14,10 +14,19 @@ var server = http.createServer(function(request, response){
 	var path = url.parse(request.url).pathname;
 	console.log('Path='+path);
 	switch(path){
-		case '/':
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write('hello world');
-			response.end(); 
+		case '/':			
+			fs.readFile(__dirname + "/index.html", function (error, data) {
+				if (error) {
+					response.writeHead(404);
+					response.write("opps this doesn't exist - 404");
+					response.end();  
+				}  
+				else{	
+					response.writeHeader(200, {"Content-Type": "text/html"});  
+					response.write(data, "utf8");
+					response.end();  
+				}
+			});
 			break;
 			
 		case '/socket.html':
@@ -46,6 +55,8 @@ var server = http.createServer(function(request, response){
 });
 
 server.listen(8001);
+console.log("Server Started in port 8001");
+
 var serv_io = io.listen(server);
 var textarea="";
 var name="";
@@ -98,49 +109,3 @@ function getCurrentTime(){
 	return dateString;
 }
 
-readGoogleDocs();
-function readGoogleDocs(){
-	//SpreadSheat url
-	//https://docs.google.com/spreadsheets/d/1fyZ0jib5ARvxWiagjPSB-0iapIe32aH5FOZioum7qyw/edit
-	var GoogleSpreadsheet = require("google-spreadsheet");
-
-	var my_sheet = new GoogleSpreadsheet('1fyZ0jib5ARvxWiagjPSB-0iapIe32aH5FOZioum7qyw');
-
-	// without auth -- read only
-	// # is worksheet id - IDs start at 1
-	/*my_sheet.getRows( 1, function(err, row_data){
-		//console.log( 'pulled in '+row_data.length + ' rows ')
-		console.log(row_data);
-	});*/
-
-	// set auth to be able to edit/add/delete
-	my_sheet.setAuth('AvnetPremierLeague@gmail.com','intel045', function(err){
-		my_sheet.getInfo( function( err, sheet_info ){
-			console.log( sheet_info.title + ' spreadsheet is loaded' );
-			// use worksheet object if you want to forget about ids
-			sheet_info.worksheets[0].getRows( function( err, rows ){
-				//rows[0].colname = 'new val';
-				//;prows[0].save();
-				//rows[0].del();
-				//console.log("rows=%j",rows[0]	);
-				
-				//Writing
-				rows[0].apple="orange"
-				rows[0].save();
-				
-				//Reading
-				console.log("row[1].apple=",rows[1].apple	);
-			});
-		});
-
-		// column names are set by google based on the first row of your sheet
-		//my_sheet.addRow( 2, { colname: 'col value'} );
-	/*
-		my_sheet.getRows( 2, {
-			start: 100,            // start index
-			num: 100            // number of rows to pull
-		}, function(err, row_data){
-			// do something...
-		}); */
-	})
-}
